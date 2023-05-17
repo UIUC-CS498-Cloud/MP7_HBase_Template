ea98422632702bc6ab8c169fa37e1ba0864f58eb
NVadde2 <112197903+NVadde2@users.noreply.github.com>
2023-05-16 23:07:29 -0500

    Update TablePartF.py

--- PythonTemplate/TablePartF.py
+++ PythonTemplate/TablePartF.py
@@ -2,6 +2,9 @@ import happybase as hb
 
 # DON'T CHANGE THE PRINT FORMAT, WHICH IS THE OUTPUT
 # OR YOU WON'T RECEIVE POINTS FROM THE GRADER
+
+#1. update the data in a particular cell using the put() method
+#2. retrieve all versions of all columns, with the most recent version coming first
                                                                                      
 row = ???
 for ???, ??? in row.items():
